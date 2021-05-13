a8134b509d7c1a1328c85a72f11140b768f559d9
DaHye Jeong <43019891+IreneJeong@users.noreply.github.com>
2021-05-13 22:23:44 +0900

    Add files via upload

---
+++ population.c
@@ -0,0 +1,43 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int get_start_size(void);
+int get_end_size(void);
+int e, s;
+
+int main(void)
+{
+    //Variable declaration
+    int ss = get_start_size();
+    int es = get_end_size();
+    int y = 0;
+    //Calculate number of years until we reach threshold & print number of years
+    do
+    {
+        ss = ss + (ss / 3) - (ss / 4);
+        y++;
+    }
+    while (ss < es);
+    printf("Years : %i\n", y);
+}
+
+//Prompt for start size
+int get_start_size(void)
+{
+    do
+    {
+        s = get_int("Start Size? : ");
+    }
+    while (s < 9);
+    return s;
+}
+//Prompt for end size
+int get_end_size(void)
+{
+    do
+    {
+        e = get_int("End size?: ");
+    }
+    while (e <= s);
+    return e;
+}
