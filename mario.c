c685980c12ef7ab6e01e5af2109ec9951064e296
DaHye Jeong <43019891+IreneJeong@users.noreply.github.com>
2021-05-16 13:10:48 +0900

    Add files via upload

--- mario.c
+++ mario.c
@@ -2,32 +2,38 @@
 #include <cs50.h>
 
 int height_int(void);
+
 int main(void){
 
     //Assign input data from user
     int n  = height_int() ;
 
     //Draw the tower
-    for(int i = 1 ; i <= n ;i++)
+    for (int i = 1; i <= n; i++)
     {
-        for(int j = 1; j <= 18; j++)
+        for (int j = 1; j <= (n * 2) + 2; j++)
         {
-            if (8 - j < i && j < 9)
+            if (n - j < i && j < n + 1)
             {
                 printf("#");
             }
-            else if (j == 9 || j == 10)
+            else if (j == n + 1 || j == n + 2)
             {
                 printf(" ");
             }
-            else if (j > 10 && j - 11 < i)
+            else if (j  > n + 1 && j - (n + 2) < i + 1)
             {
                 printf("#");
+            }
+            else if (j  > n + 1 && j - (n + 2) > i)
+            {
+                printf("");
             }
             else
             {
                 printf(" ");
             }
+
         }
         printf("\n");
     }
